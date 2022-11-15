ecef1516d19c606bb54ca19fdca48ae6af0bef83
Decyl0 <adamgolec93@gmail.com>
2022-11-15 17:09:53 +0000

    complete ticket 4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,11 +1,14 @@
 const request = require("supertest");
 const app = require("../app.js");
-const seed = require('../db/seeds/seed');
-const testData = require('../db/data/test-data/index');
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data/index");
+const db = require("../db/connection.js");
 
 beforeEach(() => {
   return seed(testData);
 });
+
+afterAll(() => db.end());
 
 describe("/api/categories", () => {
   test("GET - 200: responds with an array of category objects, each of which should have properties: slug, description", () => {
@@ -24,13 +27,56 @@ describe("/api/categories", () => {
       });
   });
 
-  test('GET - 404 - Invalid Path', () => {
+  test("GET - 404: Invalid Path", () => {
     return request(app)
       .get("/api/categoriesbadurl")
       .expect(404)
       .then((res) => {
-        expect(res.body.msg).toBe('Invalid URL');
+        expect(res.body.msg).toBe("Invalid URL");
+      });
+  });
+});
+
+describe("/api/reviews", () => {
+  test("GET - 200: responds with array of review objects", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.review.length).toBeGreaterThan(0);
+        // console.log(body.review);
+        body.review.forEach(review => { 
+          expect(review).toEqual({
+            owner: expect.any(String),
+            title: expect.any(String),
+            review_id: expect.any(Number),
+            category: expect.any(String),
+            review_img_url: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            designer: expect.any(String),
+            comment_count: expect.any(String)
+          })
+        });
       });
   });
 
+  test("GET - 200: array of reviews sorted in descending order by date", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body }) => {
+        console.log(body.review);
+        expect(body.review).toBeSortedBy("created_at", {descending: true});
+      });
+  });
+
+  test("GET - 404: Invalid Path", () => {
+    return request(app)
+      .get("/api/reviewswrongurl")
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Invalid URL");
+      });
+  });
 });
--- models/model.js
+++ models/model.js
@@ -1,6 +1,27 @@
-const db = require('../db/connection.js');
+const db = require("../db/connection.js");
 
 exports.categories = () => {
-    return db.query('SELECT * FROM categories')
+  return db.query("SELECT * FROM categories").then((result) => result.rows);
+};
+
+exports.reviews = () => {
+  return db
+    .query(
+      `
+        SELECT owner,
+        title,
+        reviews.review_id,
+        review_img_url,
+        category,
+        reviews.created_at,
+        reviews.votes,
+        designer, 
+        COUNT(comments.comment_id) AS comment_count 
+        FROM reviews 
+        LEFT JOIN comments ON comments.review_id = reviews.review_id 
+        GROUP BY reviews.review_id
+        ORDER BY created_at DESC
+      `
+    )
     .then((result) => result.rows);
 };
